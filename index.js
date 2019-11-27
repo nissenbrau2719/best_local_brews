7a806ef27c8a3f0cfe91b173c7aca651dd927ebe
Paul Nissen <52768967+nissenbrau2719@users.noreply.github.com>
2019-11-27 16:52:52 -0700

    built watchForm function to populate search form

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("connected")
+
+function getSearchArea() {
+  $('form').on('click', $('#js-findBreweries'), function() {
+    event.preventDefault();
+    console.log('get search area');
+  })
+}
+
+function watchForm() {
+  $('form').one('click', $('#js-getStarted'), function(){
+    event.preventDefault();
+    console.log('ran watchForm');
+    $('form').empty();
+    $('form').append(`
+    <fieldset>
+      <legend><h2>Let's find the best breweries in your area!</h2></legend>
+      <label for="location">Enter an address or location to search nearby:</label>
+      <input type="text" name="location" id="location" required>
+      <label for="searchRadius">How many miles are you willing to travel?:</label>
+      <input type="number" id="searchRadius" value="5" min="1" max="31" required>
+      <button type="submit" id="js-findBreweries">Find Breweries</button>
+    </fieldset>
+    `);
+    getSearchArea();
+  });
+}
+
+$(watchForm);
