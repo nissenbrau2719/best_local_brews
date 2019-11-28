f0109ebbdbedf7c41a6f53419450d0a32c49810f
Paul Nissen <52768967+nissenbrau2719@users.noreply.github.com>
2019-11-28 10:08:22 -0700

    added getParams function

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-console.log("connected")
+let searchLocation;
+let searchRadius;
+const searchForm = 
+`<fieldset>
+  <legend><h2>Let's find the best breweries in your area!</h2></legend>
+  <label for="location">Enter an address or location to search nearby:</label>
+  <input type="text" name="location" id="location" required>
+  <label for="searchRadius">How many miles are you willing to travel?:</label>
+  <input type="number" id="searchRadius" value="5" min="1" max="31" required>
+  <button type="submit" id="js-findBreweries">Find Breweries</button>
+</fieldset>`;
 
-function getSearchArea() {
-  $('form').on('click', $('#js-findBreweries'), function() {
+function getSearchParams() {
+  $('form').on('click', '#js-findBreweries', function(event) {
+    event.stopPropagation();
     event.preventDefault();
-    console.log('get search area');
-  })
+    if($('#location').val() === "") {
+      alert("Please enter an address or location");
+    } else if($('#searchRadius').val() < 1 || $('#searchRadius').val() > 31 ) {
+      alert("Please enter a search radius between 1 and 31");
+    } else {
+      searchLocation = $('#location').val();
+      console.log(searchLocation)
+      searchRadius = $('#searchRadius').val();
+      console.log(searchRadius);
+    }
+  });
 }
 
 function watchForm() {
-  $('form').one('click', $('#js-getStarted'), function(){
+  $('#js-getStarted').click(event => {
+    event.stopPropagation();
     event.preventDefault();
-    console.log('ran watchForm');
     $('form').empty();
-    $('form').append(`
-    <fieldset>
-      <legend><h2>Let's find the best breweries in your area!</h2></legend>
-      <label for="location">Enter an address or location to search nearby:</label>
-      <input type="text" name="location" id="location" required>
-      <label for="searchRadius">How many miles are you willing to travel?:</label>
-      <input type="number" id="searchRadius" value="5" min="1" max="31" required>
-      <button type="submit" id="js-findBreweries">Find Breweries</button>
-    </fieldset>
-    `);
-    getSearchArea();
+    $('form').html(searchForm);
+    getSearchParams();
   });
 }
 
